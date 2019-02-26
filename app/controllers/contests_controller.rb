b12a1b008d82d5b2fb5b405d6826cc8c2aa115bb
Leon Boutrand <leonboutrand@gmail.com>
2019-02-26 18:33:30 +0100

    everything is alright for generation of contests and players. Missing : Games generator

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,38 +1,28 @@
 class ContestsController < ApplicationController
   def show
+    @contest = Contest.find(params[:id])
   end
 
   def new
     @contest = Contest.new
+    @code = '12345678'
   end
 
   def create
     @contest = Contest.create!(contest_params)
-    p "contest params : #{contest_params}"
-    p "params : #{params}"
-    p "contest : #{@contest.title}"
-    p "contest : #{@contest}"
-    p "contest : #{@contest}"
     if @contest
-      # generate_players(@contest, params[:])
-      p 'yes'
-      generate_games(@contest.players_nb)
+      generate_games(@contest)
       redirect_to contest_path(@contest)
     else
-      p 'no'
       render :new
     end
   end
 
   private
 
-  # def generate_players(contest, players_names)
-  #   players_names.each do |player_name|
-  #     player = Player.new(name: player_name)
-  #     player.contest = contest
-  #     player.save!
-  #   end
-  # end
+  def generate_games(contest)
+    # TODO, generate games, we already have @contest.players
+  end
 
   def contest_params
     params.require(:contest).permit(:category, :title, :description, :code, :creator_id, :players_nb, :coins_init, players_attributes: [:name])
