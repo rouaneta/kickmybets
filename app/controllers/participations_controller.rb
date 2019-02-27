5a44399ed211f65c8b0481e3013590b8a889a662
Leon Boutrand <leonboutrand@gmail.com>
2019-02-27 12:20:07 +0100

    bet creation working

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,8 +1,10 @@
 class ParticipationsController < ApplicationController
   def show
+    @participation = Participation.find(params[:id])
+    @bet = Bet.new
   end
 
   def create
-    
+
   end
 end
