8bc23c7c4ace930df0b129d84d3169f446910318
Leon Boutrand <leonboutrand@gmail.com>
2019-02-27 11:19:28 +0100

    Games generated automically when contest is created, without phase and game_code

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -22,6 +22,13 @@ class ContestsController < ApplicationController
 
   def generate_games(contest)
     # TODO, generate games, we already have @contest.players
+    contest.players.shuffle.each_slice(2).to_a.each do |game_players|
+      p '---------------HEY'
+      p Game.create!(
+        player_one: game_players[0],
+        player_two: game_players[1]
+      )
+    end
   end
 
   def contest_params
