9ae057d398783a4f428ffe2eab3a40736f55df44
hugo razafindranaly <hugo.raza@gmail.com>
2019-02-28 17:42:55 +0100

    working on front navbar and homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,8 @@
 import "bootstrap";
 import playersGenerator from './players-generator';
+import menuHomepage from './menuHomepage';
+import btnHomepage from './btnHomepage';
 
 playersGenerator();
+menuHomepage();
+btnHomepage();
---
+++ app/javascript/packs/btnHomepage.js
@@ -0,0 +1,8 @@
+
+const btnHomepage = () => {
+  $('.plus-btn').click(function(){
+    $('body').toggleClass('menu-open');
+  })
+}
+
+export default btnHomepage;
