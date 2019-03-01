8e943545f9cf6cae7f3042da0892a14d64f43597
Arthur Rouanet <rouanet.arthur@gmail.com>
2019-03-01 16:52:30 +0100

    fixed bug on mailer config

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,19 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_APP_PASSWORD'],
+  domain: 'http://www.kickmybets.fun',
+  address: 'smtp.sendgrid.net',
   port: 587,
-  domain: 'gmail.com',
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_APP_PASSWORD'],
-  authentication: :login,
+  authentication: :plain,
   enable_starttls_auto: true
 }
-
-# ActionMailer::Base.smtp_settings = {
-#   user_name: ENV['SENDGRID_USERNAME'],
-#   password: ENV['SENDGRID_APP_PASSWORD'],
-#   domain: 'http://www.kickmybets.fun',
-#   address: 'smtp.sendgrid.net',
-#   port: 587,
-#   authentication: :plain,
-#   enable_starttls_auto: true
-# }
