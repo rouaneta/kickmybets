533510259b14b2b26452646d73320caf87d391f1
nathan tabone <nathan.tabone@laposte.net>
2019-03-04 19:30:02 +0100

    dashboard in working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,6 @@ class PagesController < ApplicationController
 
   def dashboard
     @user = current_user
-    # @user_contests_join = current_user.participations.map { |e| e.contest }
-    # @user_contests_create = current_user.contests
     @participation = Participation.new
   end
 end
