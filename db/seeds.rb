859a81f1aee93f447df7f43d6c0b26f031a5d932
Arthur Rouanet <rouanet.arthur@gmail.com>
2019-03-05 18:34:41 +0100

    improved seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,163 +13,163 @@ puts "Creating Users"
 users_attributes = []
 users_attributes << {
   nickname: "albane",
-  email: "albane@kickmybets.com",
+  email: "albane@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/albane.jpg'
 }
 users_attributes << {
   nickname: "alexis",
-  email: "alexis@kickmybets.com",
+  email: "alexis@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/alexis.jpg'
 }
 users_attributes << {
   nickname: "aymeric",
-  email: "aymeric@kickmybets.com",
+  email: "aymeric@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/aymeric.jpg'
 }
 users_attributes << {
   nickname: "ayoub",
-  email: "ayoub@kickmybets.com",
+  email: "ayoub@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/ayoub.jpg'
 }
 users_attributes << {
   nickname: "caroline",
-  email: "caroline@kickmybets.com",
+  email: "caroline@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/caroline.jpg'
 }
 users_attributes << {
   nickname: "chloe",
-  email: "chloe@kickmybets.com",
+  email: "chloe@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/chloe.jpg'
 }
 users_attributes << {
   nickname: "david",
-  email: "david@kickmybets.com",
+  email: "david@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/david.jpg'
 }
 users_attributes << {
   nickname: "deborah",
-  email: "deborah@kickmybets.com",
+  email: "deborah@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/deborah.jpg'
 }
 users_attributes << {
   nickname: "eleonore",
-  email: "eleonore@kickmybets.com",
+  email: "eleonore@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/eleonore.jpg'
 }
 users_attributes << {
   nickname: "emna",
-  email: "emna@kickmybets.com",
+  email: "emna@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/emna.jpg'
 }
 users_attributes << {
   nickname: "francois",
-  email: "francois@kickmybets.com",
+  email: "francois@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/francois.jpg'
 }
 users_attributes << {
   nickname: "germain",
-  email: "germain@kickmybets.com",
+  email: "germain@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/germain.jpg'
 }
 users_attributes << {
   nickname: "hugo_ple",
-  email: "hugo_ple@kickmybets.com",
+  email: "hugo_ple@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/hugo_ple.jpg'
 }
 users_attributes << {
   nickname: "hugo_raza",
-  email: "hugo_raza@kickmybets.com",
+  email: "hugo_raza@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/hugo_raza.jpg'
 }
 users_attributes << {
   nickname: "jordi",
-  email: "jordi@kickmybets.com",
+  email: "jordi@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/jordi.jpg'
 }
 users_attributes << {
   nickname: "julie",
-  email: "julie@kickmybets.com",
+  email: "julie@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/julie.jpg'
 }
 users_attributes << {
   nickname: "leon",
-  email: "leon@kickmybets.com",
+  email: "leon@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/leon.jpg'
 }
 users_attributes << {
   nickname: "maxime_berger",
-  email: "maxime_berger@kickmybets.com",
+  email: "maxime_berger@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/maxime_berger.jpg'
 }
 users_attributes << {
   nickname: "maxime_legras",
-  email: "maxime_legras@kickmybets.com",
+  email: "maxime_legras@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/maxime_legras.jpg'
 }
 users_attributes << {
   nickname: "nathan",
-  email: "nathan@kickmybets.com",
+  email: "nathan@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/nathan.jpg'
 }
 users_attributes << {
   nickname: "nicolas",
-  email: "nicolas@kickmybets.com",
+  email: "nicolas@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/nicolas.jpg'
 }
 users_attributes << {
   nickname: "nil",
-  email: "nil@kickmybets.com",
+  email: "nil@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/nil.jpg'
 }
 users_attributes << {
   nickname: "ouramdane",
-  email: "ouramdane@kickmybets.com",
+  email: "ouramdane@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/ouramdane.jpg'
 }
 users_attributes << {
   nickname: "pedro",
-  email: "pedro@kickmybets.com",
+  email: "pedro@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/pedro.jpg'
 }
 users_attributes << {
   nickname: "matthieu",
-  email: "matthieu@kickmybets.com",
+  email: "matthieu@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/matthieu.jpg'
 }
 users_attributes << {
   nickname: "kevin",
-  email: "kevin@kickmybets.com",
+  email: "kevin@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/kevin.jpg'
 }
 users_attributes << {
   nickname: "josephb",
-  email: "josephb@kickmybets.com",
+  email: "josephb@kickmybets.fun",
   password: "azertyuiop",
   picture_path: 'users/joseph.jpg'
 }
