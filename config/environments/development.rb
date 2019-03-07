a1a40dfec402b8f079b2fab24d7c4105ad89033c
Nathan <nathan.tabone@laposte.net>
2019-03-07 12:27:01 +0100

    delete css component and and replace smtp method mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default_url_options = { host: "http://www.kickmybets.fun" }
   # Verifies that versions and hashed value of the package contents in the project's package.json
