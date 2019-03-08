c2cee08095e1676cfd1e79384048279defc79da2
Arthur Rouanet <rouanet.arthur@gmail.com>
2019-03-08 12:18:32 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,243 +1,378 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 puts "Emptying database"
-User.destroy_all
+  User.destroy_all
 
 puts "Creating Users"
-users_attributes = []
-users_attributes << {
-  nickname: "albane",
-  email: "albane@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/albane.jpg'
-}
-users_attributes << {
-  nickname: "alexis",
-  email: "alexis@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/alexis.jpg'
-}
-users_attributes << {
-  nickname: "aymeric",
-  email: "aymeric@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/aymeric.jpg'
-}
-users_attributes << {
-  nickname: "ayoub",
-  email: "ayoub@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/ayoub.jpg'
-}
-users_attributes << {
-  nickname: "caroline",
-  email: "caroline@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/caroline.jpg'
-}
-users_attributes << {
-  nickname: "chloe",
-  email: "chloe@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/chloe.jpg'
-}
-users_attributes << {
-  nickname: "david",
-  email: "david@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/david.jpg'
-}
-users_attributes << {
-  nickname: "deborah",
-  email: "deborah@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/deborah.jpg'
-}
-users_attributes << {
-  nickname: "eleonore",
-  email: "eleonore@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/eleonore.jpg'
-}
-users_attributes << {
-  nickname: "emna",
-  email: "emna@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/emna.jpg'
-}
-users_attributes << {
-  nickname: "francois",
-  email: "francois@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/francois.jpg'
-}
-users_attributes << {
-  nickname: "germain",
-  email: "germain@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/germain.jpg'
-}
-users_attributes << {
-  nickname: "hugo_ple",
-  email: "hugo_ple@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/hugo_ple.jpg'
-}
-users_attributes << {
-  nickname: "hugo_raza",
-  email: "hugo_raza@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/hugo_raza.jpg'
-}
-users_attributes << {
-  nickname: "jordi",
-  email: "jordi@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/jordi.jpg'
-}
-users_attributes << {
-  nickname: "julie",
-  email: "julie@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/julie.jpg'
-}
-users_attributes << {
-  nickname: "leon",
-  email: "leon@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/leon.jpg'
-}
-users_attributes << {
-  nickname: "maxime_berger",
-  email: "maxime_berger@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/maxime_berger.jpg'
-}
-users_attributes << {
-  nickname: "maxime_legras",
-  email: "maxime_legras@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/maxime_legras.jpg'
-}
-users_attributes << {
-  nickname: "nathan",
-  email: "nathan@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/nathan.jpg'
-}
-users_attributes << {
-  nickname: "nicolas",
-  email: "nicolas@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/nicolas.jpg'
-}
-users_attributes << {
-  nickname: "nil",
-  email: "nil@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/nil.jpg'
-}
-users_attributes << {
-  nickname: "ouramdane",
-  email: "ouramdane@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/ouramdane.jpg'
-}
-users_attributes << {
-  nickname: "arthur",
-  email: "arthur@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/arthur.jpg'
-}
-users_attributes << {
-  nickname: "pedro",
-  email: "pedro@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/pedro.jpg'
-}
-users_attributes << {
-  nickname: "matthieu",
-  email: "matthieu@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/matthieu.jpg'
-}
-users_attributes << {
-  nickname: "kevin",
-  email: "kevin@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/kevin.jpg'
-}
-users_attributes << {
-  nickname: "josephb",
-  email: "josephb@kickmybets.fun",
-  password: "azertyuiop",
-  picture_path: 'users/joseph.jpg'
-}
-User.create!(users_attributes)
-user = User.where(nickname: "matthieu").first
+  albane = User.create! (
+    {
+    nickname: "albane",
+    email: "albane@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/albane.jpg'
+    }
+  )
+  alexis = User.create! (
+    {
+    nickname: "alexis",
+    email: "alexis@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/alexis.jpg'
+    }
+  )
+  aymeric = User.create! (
+    {
+    nickname: "aymeric",
+    email: "aymeric@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/aymeric.jpg'
+    }
+  )
+  ayoub = User.create! (
+    {
+    nickname: "ayoub",
+    email: "ayoub@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/ayoub.jpg'
+    }
+  )
+  caroline = User.create! (
+    {
+    nickname: "caroline",
+    email: "caroline@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/caroline.jpg'
+    }
+  )
+  chloe = User.create! (
+    {
+    nickname: "chloe",
+    email: "chloe@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/chloe.jpg'
+    }
+  )
+  david = User.create! (
+    {
+    nickname: "david",
+    email: "david@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/david.jpg'
+    }
+  )
+  deborah = User.create! (
+    {
+    nickname: "deborah",
+    email: "deborah@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/deborah.jpg'
+    }
+  )
+  eleonore = User.create! (
+    {
+    nickname: "eleonore",
+    email: "eleonore@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/eleonore.jpg'
+    }
+  )
+  emna = User.create! (
+    {
+    nickname: "emna",
+    email: "emna@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/emna.jpg'
+    }
+  )
+  francois = User.create! (
+    {
+    nickname: "francois",
+    email: "francois@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/francois.jpg'
+    }
+  )
+  germain = User.create! (
+    {
+    nickname: "germain",
+    email: "germain@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/germain.jpg'
+    }
+  )
+  hugo_ple = User.create! (
+    {
+    nickname: "hugo_ple",
+    email: "hugo_ple@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/hugo_ple.jpg'
+    }
+  )
+  hugo_raza = User.create! (
+    {
+    nickname: "hugo_raza",
+    email: "hugo_raza@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/hugo_raza.jpg'
+    }
+  )
+  jordi = User.create! (
+    {
+    nickname: "jordi",
+    email: "jordi@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/jordi.jpg'
+    }
+  )
+  julie = User.create! (
+    {
+    nickname: "julie",
+    email: "julie@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/julie.jpg'
+    }
+  )
+  leon = User.create! (
+    {
+    nickname: "leon",
+    email: "leon@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/leon.jpg'
+    }
+  )
+  maxime_berger = User.create! (
+    {
+    nickname: "maxime_berger",
+    email: "maxime_berger@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/maxime_berger.jpg'
+    }
+  )
+  maxime_legras = User.create! (
+    {
+    nickname: "maxime_legras",
+    email: "maxime_legras@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/maxime_legras.jpg'
+    }
+  )
+  nathan = User.create! (
+    {
+    nickname: "nathan",
+    email: "nathan@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/nathan.jpg'
+    }
+  )
+  nicolas = User.create! (
+    {
+    nickname: "nicolas",
+    email: "nicolas@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/nicolas.jpg'
+    }
+  )
+  nil_b = User.create! (
+    {
+    nickname: "nil_b",
+    email: "nil@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/nil.jpg'
+    }
+  )
+  ouramdane = User.create! (
+    {
+    nickname: "ouramdane",
+    email: "ouramdane@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/ouramdane.jpg'
+    }
+  )
+  arthur = User.create! (
+    {
+    nickname: "arthur",
+    email: "arthur@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/arthur.jpg'
+    }
+  )
+  pedro = User.create! (
+    {
+    nickname: "pedro",
+    email: "pedro@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/pedro.jpg'
+    }
+  )
+  matthieu = User.create! (
+    {
+    nickname: "matthieu",
+    email: "matthieu@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/matthieu.jpg'
+    }
+  )
+  kevin = User.create! (
+    {
+    nickname: "kevin",
+    email: "kevin@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/kevin.jpg'
+    }
+  )
+  joseph = User.create! (
+    {
+    nickname: "joseph",
+    email: "joseph@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/joseph.jpg'
+    }
+  )
+  boris = User.create! (
+    {
+    nickname: "boris",
+    email: "boris@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/boris.jpg'
+    }
+  )
+  dhh = User.create! (
+    {
+    nickname: "dhh",
+    email: "dhh@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/dhh.jpg'
+    }
+  )
+  tenderlove = User.create! (
+    {
+    nickname: "tenderlove",
+    email: "tenderlove@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/tenderlove.jpg'
+    }
+  )
+  toto = User.create! (
+    {
+    nickname: "toto",
+    email: "toto@kickmybets.fun",
+    password: "azertyuiop",
+    picture_path: 'users/toto.jpg'
+    }
+  )
 
 puts "Creating Contests"
-Contest.create!(
-  {
-  picture_path: 'contests/beerpong.jpg',
-  title: "Beer-Pong",
-  coins_init: 100,
-  category: "cup",
-  code: "eByTvh8ne",
-  players_nb: 2,
-  creator: user,
-  players_attributes: [{name: "Toto"}, {name: "Titi"}]
-  }
-)
-Contest.create!(
-  {
-  picture_path: 'contests/tank_race.jpg',
-  title: "Tank race",
-  coins_init: 100,
-  category: "cup",
-  code: "vVQ2R8v7m",
-  players_nb: 2,
-  creator: user,
-  players_attributes: [{name: "Toto"}, {name: "Titi"}]
-  }
-)
-contest = Contest.create!(
-  {
-  picture_path: 'contests/pingpong.jpg',
-  title: "Ping-Pong",
-  coins_init: 100,
-  category: "cup",
-  code: "zEhv8YavQ",
-  players_nb: 8,
-  creator: user,
-  players_attributes: [{name: "eleonore"}, {name: "matthieu"}, {name: "leon"}, {name: "francois"}, {name: "david"}, {name: "aymeric"}, {name: "joseph"}, {name: "benjamin"}]
-  }
-)
+  Contest.create!(
+    {
+    picture_path: 'contests/boulescarrees.jpg',
+    title: "Boules carrées",
+    coins_init: 100,
+    category: "cup",
+    code: "eByTvh8ne",
+    players_nb: 2,
+    creator: arthur,
+    players_attributes: [{name: "Toto"}, {name: "Titi"}]
+    }
+  )
+  pingpong = Contest.create!(
+    {
+    picture_path: 'contests/pingpong.jpg',
+    title: "Ping-Pong",
+    coins_init: 100,
+    category: "cup",
+    code: "zEhv8YavQ",
+    players_nb: 8,
+    creator: matthieu,
+    players_attributes: [{name: "eleonore"}, {name: "francois"}, {name: "tenderlove"}, {name: "kevin"}, {name: "toto"}, {name: "joseph"}, {name: "boris"}, {name: "matthieu"}]
+    }
+  )
+  Contest.create!(
+    {
+    picture_path: 'contests/hotdog_contest.jpg',
+    title: "Hot Dog contest",
+    coins_init: 100,
+    category: "cup",
+    code: "LxYB7TdV",
+    players_nb: 2,
+    creator: arthur,
+    players_attributes: [{name: "Toto"}, {name: "Titi"}]
+    }
+  )
+  Contest.create!(
+    {
+    picture_path: 'contests/virtual_dance.jpg',
+    title: "Dance Dance Revolution",
+    coins_init: 100,
+    category: "cup",
+    code: "ihM8nuiy",
+    players_nb: 2,
+    creator: arthur,
+    players_attributes: [{name: "Toto"}, {name: "Titi"}]
+    }
+  )
+  Contest.create!(
+    {
+    picture_path: 'contests/dance.jpg',
+    title: "Danse folklorique",
+    coins_init: 100,
+    category: "cup",
+    code: "rq4HJtur",
+    players_nb: 2,
+    creator: arthur,
+    players_attributes: [{name: "Toto"}, {name: "Titi"}]
+    }
+  )
+  Contest.create!(
+    {
+    picture_path: 'contests/diabolo.jpg',
+    title: "Diabolo entre potos",
+    coins_init: 100,
+    category: "cup",
+    code: "TnuTkFz7",
+    players_nb: 2,
+    creator: arthur,
+    players_attributes: [{name: "Toto"}, {name: "Titi"}]
+    }
+  )
 
-Player.where(name: "eleonore").first.update(picture_path: "users/eleonore.jpg")
-Player.where(name: "matthieu").first.update(picture_path: "users/matthieu.jpg")
-Player.where(name: "leon").first.update(picture_path: "users/leon.jpg")
-Player.where(name: "francois").first.update(picture_path: "users/francois.jpg")
-Player.where(name: "david").first.update(picture_path: "users/david.jpg")
-Player.where(name: "aymeric").first.update(picture_path: "users/aymeric.jpg")
-Player.where(name: "joseph").first.update(picture_path: "users/joseph.jpg")
-Player.where(name: "benjamin").first.update(picture_path: "users/benjamin.jpg")
+puts "Updating players pictures"
+  Player.where(name: "eleonore").first.update(picture_path: "users/eleonore.jpg")
+  Player.where(name: "matthieu").first.update(picture_path: "users/matthieu.jpg")
+  Player.where(name: "francois").first.update(picture_path: "users/francois.jpg")
+  Player.where(name: "joseph").first.update(picture_path: "users/joseph.jpg")
+  Player.where(name: "tenderlove").first.update(picture_path: "users/tenderlove.jpg")
+  Player.where(name: "kevin").first.update(picture_path: "users/kevin.jpg")
+  Player.where(name: "toto").first.update(picture_path: "users/toto.jpg")
+  Player.where(name: "boris").first.update(picture_path: "users/boris.jpg")
 
 puts"Creating participations"
-participation = Participation.create!(user: user, contest: contest, betcoins: contest.coins_init)
+  participation_albane = Participation.create!(user: albane, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_alexis = Participation.create!(user: alexis, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_aymeric = Participation.create!(user: aymeric, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_ayoub = Participation.create!(user: ayoub, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_caroline = Participation.create!(user: caroline, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_chloe = Participation.create!(user: chloe, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_david = Participation.create!(user: david, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_deborah = Participation.create!(user: deborah, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_emna = Participation.create!(user: emna, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_germain = Participation.create!(user: germain, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_hugo_ple = Participation.create!(user: hugo_ple, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_hugo_raza = Participation.create!(user: hugo_raza, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_jordi = Participation.create!(user: jordi, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_julie = Participation.create!(user: julie, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_leon = Participation.create!(user: leon, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_maxime_berger = Participation.create!(user: maxime_berger, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_maxime_legras = Participation.create!(user: maxime_legras, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_nathan = Participation.create!(user: nathan, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_nicolas = Participation.create!(user: nicolas, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_nil_b = Participation.create!(user: nil_b, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_ouramdane = Participation.create!(user: ouramdane, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_pedro = Participation.create!(user: pedro, contest: pingpong, betcoins: pingpong.coins_init)
+  participation_dhh = Participation.create!(user: dhh, contest: pingpong, betcoins: pingpong.coins_init)
 
 puts "Creating Events"
-
-event = Event.create!(
-  participation: participation,
-  title: "Fracture du mental pour François en demi-finale",
-  choice_one: "Fracture",
-  choice_two: "Pas de fracture, man!"
-)
+  event = Event.create!(
+    participation: participation_nathan,
+    title: "Fracture du mental pour François en demi-finale",
+    choice_one: "Fracture",
+    choice_two: "Pas de fracture, man!"
+  )
 
